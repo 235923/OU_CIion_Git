541166c2b32030d475bdbd99e9f8b409f48135fd
235923 <235923@edu.p.lodz.pl>
2021-02-01 17:23:18 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CIion_Git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CIion_Git.dir/main.cpp.obj"
+  "OU_CIion_Git.exe"
+  "OU_CIion_Git.exe.manifest"
+  "OU_CIion_Git.pdb"
+  "libOU_CIion_Git.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CIion_Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_CIion_Git.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_CIion_Git.
+# This may be replaced when dependencies are built.
